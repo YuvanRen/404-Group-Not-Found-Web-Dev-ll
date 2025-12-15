6efde656e86c490ea1bdc93d9e6933c443a4a042
james-kaddissi <jameskaddissi@gmail.com>
2025-12-15 15:24:51 -0500

    Dockerfile config fix

--- client/src/utils/resumeApi.js
+++ client/src/utils/resumeApi.js
@@ -1,4 +1,4 @@
-const API_BASE = process.env.REACT_APP_API_URL || "";
+const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:4000";
 
 async function extractError(res) {
   const ct = res.headers.get("content-type") || "";
